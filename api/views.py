f90995da90980bad091ad5c39f08dec1b258c3da
Ana Barbosa <anael.noki@gmail.com>
2025-02-15 19:35:17 -0300

    feat: adiciona uma melhor listagem para spells

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Spell
 from .serializers import SpellSerializer
 
@@ -11,3 +12,19 @@ class SpellViewSet(viewsets.ModelViewSet):
     serializer_class = SpellSerializer
     lookup_field = "index"
     http_method_names = ["get"]
+    pagination_class = None
+
+    def list(self, request, *args, **kwargs):
+        spells = self.get_queryset()
+        count = spells.count()
+
+        results = [
+            {
+                "index": spell.index,
+                "name": spell.name,
+                "url": f"/api/spells/{spell.index}",
+            }
+            for spell in spells
+        ]
+
+        return Response({"count": count, "results": results})
